769e5573ac6cc507e13cf4a899d3a2480306d9f3
Icontol <119477979+Icontol@users.noreply.github.com>
2023-01-21 08:27:45 +0700

    Update payment.js

--- plugins/payment.js
+++ plugins/payment.js
@@ -2,30 +2,29 @@ import fetch from 'node-fetch'
 
 //Plugin By Xynoz!!
 let handler = async (m, { conn, usedPrefix }) => {
-  const ultah = new Date('November 4 2022 00:00:01')
+  const ultah = new Date('September 10 2023 00:00:01')
     const sekarat = new Date().getTime() 
     const Kurang = ultah - sekarat
     const ohari = Math.floor( Kurang / (1000 * 60 * 60 * 24));
     const ojam = Math.floor( Kurang % (1000 * 60 * 60 * 24) / (1000 * 60 * 60))
     const onet = Math.floor( Kurang % (1000 * 60 * 60) / (1000 * 60))
     const detek = Math.floor( Kurang % (1000 * 60) / 1000)
-    let audio = `./mp3/nexep.opus`
+  let vn = `https://github.com/Icontol/kontolbapakkaupecah/blob/master/mp3/ivan.mp3?raw=true`
   conn.sendButton(m.chat, `
 ┏━ꕥ〔 *Donasi • Dana* 〕ꕥ━⬣
-┃✾ Gopay  [-]
-┃✾ Dana  [-]
-┃✾ Saweria  [-]
+┃✾ Gopay  [6285876830674]
+┃✾ Dana  [6285876830674]
 ┗━━━━ꕥ
 ┏━━ꕥ〔 *NOTE* 〕ꕥ━⬣
-┃ Ingin donasi? Wa.me/628812743180
+┃ Ingin donasi? Wa.me/6285876830674
 ┃ _Hasil donasi akan digunakan buat sewa_
 ┃ _atau beli *RDP/VPS* agar bot bisa jalan_
 ┃ _24jam tanpa kendala_
 ┗━━━━ꕥ
 `.trim(), wm, [['Owner',`/owner`]],m) // Tambah sendiri kalo mau
-conn.sendFile(m.chat, audio, './mp3/nexep.opus', null, m, true, {
+conn.sendFile(m.chat, vn, 'haori.mp3', null, m, true, {
 type: 'audioMessage', 
-ptt: true, contextInfo:{ externalAdReply: { title: `💌 Ultah Owner : ${ohari} Hari ${ojam} Jam ${onet} Menit ${detek} Detik`, body: `Follow Tiktok My Bestie`, sourceUrl: 'https://www.tiktok.com/@nexep1', thumbnail: await (await fetch('https://i.ibb.co/3RSVFP3/bawah.png')).buffer(),}} 
+ptt: true, contextInfo:{ externalAdReply: { title: `💌 Ultah Owner : ${ohari} Hari ${ojam} Jam ${onet} Menit ${detek} Detik`, body: `Join yuk`, sourceUrl: 'https://chat.whatsapp.com/EPmjuw9X1wZDGX1p4NCV1e', thumbnail: await (await fetch('https://ibb.co/3R2fQCT')).buffer(),}} 
      }) 
 }
 handler.command = /^(pay|payment|bayar)$/i
