0b34e73c73e9264bc559b12cd4e886d3edd40054
Icontol <119477979+Icontol@users.noreply.github.com>
2023-01-21 07:55:38 +0700

    Update downloader-tiktok.js

--- plugins/downloader-tiktok.js
+++ plugins/downloader-tiktok.js
@@ -17,7 +17,7 @@ let handler = async (m, { conn, args, usedPrefix, command }) => {
 }
 handler.help = ['tiktok', 'tiktok', 'tiktokdl'].map(v => v + ' <url>')
 handler.tags = ['downloader']
-handler.command = /^(tik(tok)?(tok)?(dl)?)$/i
+handler.command = /^(tt|tik(tok)?(tok)?(dl)?)$/i
 handler.level = 40
 
 export default handler
